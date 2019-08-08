52efda996a269faa275c9f9a9e46541455106d1f
Ana Amaro <amarovazquezana@gmail.com>
2019-08-08 15:30:44 +0200

    Initial commit

---
+++ _src/assets/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+//console.log('>> Ready :)');
+//URl de la api externa con la que queremos conectar//
+const urlApi = '';
+
+//función para conectar con la api externa y solicitar los datos //
+function getLoquesea() {
+  fetch(urlApi)
+    .then(response => response.json())
+    .then(data => showURL(data))
+    // eslint-disable-next-line no-console
+    .catch(error => console.log(error));
+}
+
+getLoquesea();
+
+function showUrl(data) {
+    if (data.succes) {
+        
+    }
+}
+*/
+
