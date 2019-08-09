cfdb245843c030a0f3c4f8a2b927475c5e2e95c4
Ana Amaro <amarovazquezana@gmail.com>
2019-08-09 10:28:12 +0200

    fetch api ok. Filter by name ok

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,67 +1,21 @@
 'use strict';
 
-// constantes para el input & button de búsqueda //
-const inputSearch = document.querySelector('#input-tv');
+// Declaro las constantes del html que voy a utilizar //
+const inputSearch = document.querySelector('.js-input');
 const buttonSearch = document.querySelector('.js-search_btn');
-const resultShows = document.querySelector('.result_shows');
+const resultContainer = document.querySelector('.result_shows');
 const favShows = document.querySelector('.shows_fav');
 
-let shows = [];
-const favourites = [];
-let favShowsArr = JSON.parse(localStorage.getItem('favShowsArr'));
+//let resultShows = [];
+//const favourites = [];
+//let favShowsArr = JSON.parse(localStorage.getItem('favShowsArr'));
 
 // función para conectar con la api externa y solicitar los datos //
 function getUrlTv() {
-  fetch(`http://api.tvmaze.com/search/shows?q=${inputSearch}`)
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputSearch.value}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      data = formatData(data);
-      saveDataInShows(data);
-      paintShows();
-      listenShows();
-      setShowsIntoLocalStorage();
+      console.log(data[0].show.name);
     });
 }
-
-// FORMATEAMOS //
-function formatData(data) {
-  const result = [];
-  for (const show of data.shows) {
-    result.push({
-      name: show.name,
-      image: show.image
-    });
-  }
-  // eslint-disable-next-line no-console
-  console.log(result);
-  return result;
-}
-
-// GUARDAMOS //
-
-function saveDataInShows(data) {
-  shows = data;
-}
-
-// PINTAMOS //
-
-function paintShows() {
-  resultShows.innerHTML = '';
-  for (let showIndex = 0; showIndex < shows.length; showIndex++) {
-    resultShows.innerHTML += `<li class="show_item ${getFavClassName(
-      showIndex
-    )} ${getFilterClassName(showIndex)} js-shows" data-index="${showIndex}">`;
-    resultShows.innerHTML += `<p class="shows_name">${
-      shows[showIndex].name
-    }</p>`;
-    resultShows.innerHTML += '<ul class="shows_images">';
-    for (const image of shows[showIndex].images) {
-      resultShows.innerHTML += `<li class="shows_images" style="background: #${image}"></li>`;
-    }
-    resultShows.innerHTML += '</ul>';
-    resultShows.innerHTML += '</li>';
-  }
-}
-
 buttonSearch.addEventListener('click', getUrlTv);
